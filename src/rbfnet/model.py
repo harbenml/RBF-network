10092dd1c65d4a7f1cc72e0b8efdd623dfe493aa
harbenml <harbenml@pm.me>
2021-01-20 19:05:35 +0100

    add fit method to model

--- src/rbfnet/model.py
+++ src/rbfnet/model.py
@@ -7,6 +7,11 @@ class Model:
     def __init__(self):
         self.parameters: List[float]
 
+    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
+        # least squares estimation
+        w_hat = np.linalg.pinv(X) @ y
+        self.parameters = w_hat
+
     def predict(self, X: np.ndarray) -> np.ndarray:
         w = self.parameters
         y = X @ w
