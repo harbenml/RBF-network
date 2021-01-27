bad65415dde1affd4af1b5c18d2984353b6fce66
harbenml <harbenml@pm.me>
2021-01-27 19:06:49 +0100

    add class Partitioning in model.py

--- src/rbfnet/model.py
+++ src/rbfnet/model.py
@@ -1,18 +1,30 @@
-from typing import List, Tuple
+from typing import List, NamedTuple, Tuple
 
 import numpy as np
 
 
+class Partitioning(NamedTuple):
+    """Holds the parameters of the radial basis functions (rbf)
+    
+    rbf_centers (np.ndarray): center coordinate of each rbf
+    rbf_std (np.ndarray): standard deviations to construct the rbfs
+    """
+
+    rbf_centers: np.ndarray
+    rbf_std: np.ndarray
+
+
 class Model:
     def __init__(self):
-        self.parameters: List[float]
+        self.linear_params: np.ndarray
+        self.nonlin_params: Partitioning
 
     def fit(self, X: np.ndarray, y: np.ndarray) -> None:
         # least squares estimation
         w_hat = np.linalg.pinv(X) @ y
-        self.parameters = w_hat
+        self.linear_params = w_hat
 
     def predict(self, X: np.ndarray) -> np.ndarray:
-        w = self.parameters
+        w = self.linear_params
         y = X @ w
         return y
