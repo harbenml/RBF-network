1edc48caa75d23c683746a2387b1aa7fd8d5b573
harbenml <harbenml@pm.me>
2021-01-30 11:59:41 +0100

    add docstrings and annotations to rbf

--- src/rbfnet/rbf_kernel.py
+++ src/rbfnet/rbf_kernel.py
@@ -1,14 +1,39 @@
-def calculate_radial_basis_function_matrix(x, centers, stds):
-    # x: (#samples, #dimensions)
-    # centers: (#neurons, #dimensions)
-    # stds: (#neurons, #dimensions)
-    return np.exp(-0.5 * np.square((x - centers) / stds))
+import numpy as np
+
+
+def calculate_radial_basis_function_matrix(
+    x: np.ndarray, centers: np.ndarray, stds: np.ndarray
+) -> np.ndarray:
+    """Calculates the radial basis functions (RBFs) given the parameters for each
+       neuron.
+
+    Args:
+        x (np.ndarray): input samples with dim [#samples, #dimensions]
+        centers (np.ndarray): the centers of each Gaussian function  [#neurons, #dimensions]
+        stds (np.ndarray): the standard deviations of each Gaussian function  [#neurons, #dimensions]
+
+    Returns:
+        rbf_matrix (np.ndarray): 2D numpy array containing the RBF values [#samples, #neurons]
+    """
+    rbf_matrix = np.exp(-0.5 * np.square((x - centers) / stds))
+    return rbf_matrix
 
 
 def normalize_rbf(rbf_matrix):
-    # rbf_matrix: (#samples, #neurons)
+    """Normalizes each rbf neuron with the sum of all neuron values to from a
+       `partition of unity`. 
+       
+    Args:
+        rbf_matrix (np.ndarray): 2D numpy array containing the RBF values [#samples, #neurons]
+
+    Returns:
+        rbf_matrix_norm (np.ndarray): the normalized RBF values [#samples, #neurons]
+    """
     column_sum = np.sum(rbf_matrix, axis=-1)
     rbf_matrix_norm = np.zeros(rbf_matrix.shape)
     for i in range(rbf_matrix.shape[1]):
-        rbf_matrix_norm[:, i] = rbf_matrix[:, i] / column_sum
+        rbf_matrix_norm[:, i] = (
+            rbf_matrix[:, i] / column_sum
+        )  # TODO: is it possible to vectorize this?
     return rbf_matrix_norm
+
